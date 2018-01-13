66a8b01754f695f054ff33e0def04e9e54338548
Adriano Henrique <adriano.henriquesds@gmail.com>
2018-01-12 22:52:33 -0200

    Testando a primeira classe de Domínio Categoria ;

--- src/main/java/br/com/inovacenter/cursomc/resources/CategoriaResource.java
+++ src/main/java/br/com/inovacenter/cursomc/resources/CategoriaResource.java
@@ -1,16 +1,24 @@
 package br.com.inovacenter.cursomc.resources;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.inovacenter.cursomc.entity.Categoria;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {
 	
 	@RequestMapping(method = RequestMethod.GET)
-	public String listar() {
-		return "REST está funcionando";
+	public List<Categoria> listar() {
+		Categoria categoria1 = new Categoria(1, "Informática");
+		Categoria categoria2 = new Categoria(2, "Escritório");
+		List<Categoria> categorias = Arrays.asList(categoria1, categoria2);
+		return categorias;
 	}
 
 }
