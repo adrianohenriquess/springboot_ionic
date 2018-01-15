02a22415eff44df2c80abf986f3ad61b4aca8c48
Adriano Henrique <adriano.henriquesds@gmail.com>
2018-01-15 15:36:32 -0200

    Paginação com parametros opcionais na requisição

--- src/main/java/br/com/inovacenter/cursomc/CursomcApplication.java
+++ src/main/java/br/com/inovacenter/cursomc/CursomcApplication.java
@@ -67,10 +67,14 @@ public class CursomcApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... arg0) throws Exception {
-		Categoria categoria1 = new Categoria();
-		categoria1.setNome("Informática");
-		Categoria categoria2 = new Categoria();
-		categoria2.setNome("Escritório");
+		Categoria categoria1 = new Categoria(null, "Informática");
+		Categoria categoria2 = new Categoria(null, "Escritório");
+		Categoria categoria3 = new Categoria(null, "Cama, mesa e banho");
+		Categoria categoria4 = new Categoria(null, "Eletrônicos");
+		Categoria categoria5 = new Categoria(null, "Jardinagem");
+		Categoria categoria6 = new Categoria(null, "Decoração");
+		Categoria categoria7 = new Categoria(null, "Perfumaria");
+		
 		
 		Produto produto1 = new Produto(null, "Computador", 2000.00);
 		Produto produto2 = new Produto(null, "Impressora", 800.00);
@@ -83,7 +87,7 @@ public class CursomcApplication implements CommandLineRunner {
 		produto2.getCategorias().addAll(Arrays.asList(categoria1, categoria2));
 		produto3.getCategorias().add(categoria1);
 				
-		categoriaRepository.save(Arrays.asList(categoria1, categoria2));
+		categoriaRepository.save(Arrays.asList(categoria1, categoria2, categoria3, categoria4, categoria5, categoria6, categoria7));
 		produtoRepository.save(Arrays.asList(produto1, produto2, produto3));
 		
 		Estado estado1 = new Estado(null, "Mina Gerais");
