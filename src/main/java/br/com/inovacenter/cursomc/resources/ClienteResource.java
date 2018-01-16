ebd9a968f2c324327dd6698e1305230da712d569
Adriano Henrique <adriano.henriquesds@gmail.com>
2018-01-15 22:14:42 -0200

    Inserindo novo cliente com post

--- src/main/java/br/com/inovacenter/cursomc/resources/ClienteResource.java
+++ src/main/java/br/com/inovacenter/cursomc/resources/ClienteResource.java
@@ -1,5 +1,6 @@
 package br.com.inovacenter.cursomc.resources;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -14,8 +15,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import br.com.inovacenter.cursomc.dto.ClienteDTO;
+import br.com.inovacenter.cursomc.dto.ClienteNewDTO;
 import br.com.inovacenter.cursomc.entity.Cliente;
 import br.com.inovacenter.cursomc.services.ClienteService;
 
@@ -30,6 +33,18 @@ public class ClienteResource {
 	public ResponseEntity<Cliente> find(@PathVariable Integer id) {
 		Cliente cliente = clienteService.find(id);
 		return ResponseEntity.ok().body(cliente);
+	}
+	
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<Void> insert(@Valid @RequestBody ClienteNewDTO clienteNewDTO) {
+		Cliente cliente = clienteService.fromDTO(clienteNewDTO);
+		cliente = clienteService.insert(cliente);
+		URI uri = ServletUriComponentsBuilder
+						.fromCurrentRequest()
+						.path("/{id}")
+						.buildAndExpand(cliente.getId())
+						.toUri();
+		return ResponseEntity.created(uri).build();
 	}
 	
 	@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
--- src/main/java/br/com/inovacenter/cursomc/services/ClienteService.java
+++ src/main/java/br/com/inovacenter/cursomc/services/ClienteService.java
@@ -10,8 +10,14 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import br.com.inovacenter.cursomc.dto.ClienteDTO;
+import br.com.inovacenter.cursomc.dto.ClienteNewDTO;
+import br.com.inovacenter.cursomc.entity.Cidade;
 import br.com.inovacenter.cursomc.entity.Cliente;
+import br.com.inovacenter.cursomc.entity.Endereco;
+import br.com.inovacenter.cursomc.entity.enums.TipoCliente;
+import br.com.inovacenter.cursomc.repositories.CidadeRepository;
 import br.com.inovacenter.cursomc.repositories.ClienteRepository;
+import br.com.inovacenter.cursomc.repositories.EnderecoRepository;
 import br.com.inovacenter.cursomc.services.exceptions.DataIntegrityException;
 import br.com.inovacenter.cursomc.services.exceptions.ObjectNotFoundException;
 
@@ -20,6 +26,12 @@ public class ClienteService {
 	
 	@Autowired
 	private ClienteRepository clienteRepository;
+	
+	@Autowired
+	private CidadeRepository cidadeRepository;
+	
+	@Autowired
+	private EnderecoRepository enderecoRepository;
 	
 	public Cliente find(Integer id) {
 		Cliente obj = clienteRepository.findOne(id);
@@ -60,5 +72,29 @@ public class ClienteService {
 	private void updateData(Cliente clienteNovo, Cliente cliente) {
 		clienteNovo.setNome(cliente.getNome());
 		clienteNovo.setEmail(cliente.getEmail());
+	}
+
+	public Cliente insert(Cliente cliente) {
+		cliente.setId(null);
+		cliente = clienteRepository.save(cliente);
+		enderecoRepository.save(cliente.getEnderecos());
+		return cliente;
+	}
+
+	public Cliente fromDTO(ClienteNewDTO clienteNewDTO) {
+		Cliente cliente = new Cliente(null, clienteNewDTO.getNome(), clienteNewDTO.getEmail(), clienteNewDTO.getCpfCnpj(), TipoCliente.toEnum(clienteNewDTO.getTipoCliente()));
+		Cidade cidade = cidadeRepository.findOne(clienteNewDTO.getCidadeId());
+		Endereco endereco = new Endereco(null, clienteNewDTO.getLogradouro(), clienteNewDTO.getNumero(), clienteNewDTO.getComplemento(), 
+											clienteNewDTO.getBairro(), clienteNewDTO.getCep(), cliente, cidade);
+		cliente.getEnderecos().add(endereco);
+		cliente.getTelefones().add(clienteNewDTO.getTelefone1());
+		if (clienteNewDTO.getTelefone2() != null) {
+			cliente.getTelefones().add(clienteNewDTO.getTelefone2());
+		}
+		
+		if (clienteNewDTO.getTelefone3() != null) {
+			cliente.getTelefones().add(clienteNewDTO.getTelefone3());
+		}
+		return cliente;
 	}
 }
